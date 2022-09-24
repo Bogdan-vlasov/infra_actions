e70b40b6ad7d2e881a61ed9a6bc6fb7f28c5e448
Sprngtm4htlr <avraamchichikov@yandex.ru>
2022-09-24 12:10:12 +0300

    fixed for tests

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,8 +5,3 @@ urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
